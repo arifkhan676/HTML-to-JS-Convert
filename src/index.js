77059d16d02529b0313a71fc77003a6c0ecb019e
arifkhan676 <arifulhasan5500@gmail.com>
2023-02-19 15:45:29 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var h1=document.createElement("h1");
+h1.innerHTML="Hello world";
+document.getElementById("root").appendChild(h1);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var div=document.createElement("div");
+div.classList.add("row");
+
+var h2=document.createElement("h2");
+h2.innerHTML="I am h2";
+
+var p=document.createElement("p");
+p.innerHTML="I am p";
+
+div.appendChild(h2);
+div.appendChild(p);
+
+const parentElement=document.getElementById("root");
+parentElement.appendChild(div);
+
+
+
+const nav = document.createElement("nav");
+const ul= document.createElement("ul");
+
+//create an array link objects
+const links = [
+  {text: "Google", href:"https://www.google.com"},
+  {text: "twitter", href:"https://www.twitter.com"}
+];
+
+links.forEach(link => {
+  const list = document.createElement("li");
+   const aElement = document.createElement("a");
+
+   aElement.textContent=link.text;
+   aElement.href=link.href; 
+
+list.appendChild(aElement);
+ul.appendChild(list);
+} );
+
+nav.appendChild(ul);
+const navShow= document.getElementById("root");
+navShow.appendChild(nav);
+
